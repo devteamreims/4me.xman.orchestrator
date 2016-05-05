8d0948968c6f681e94840a421cd036bccf2999b0
Benjamin Beret <benjamin.beret@gmail.com>
2016-05-05 12:37:35 +0200

    Cleanup positions state tree when a flight is removed

--- src/reducers/positions.js
+++ src/reducers/positions.js
@@ -1,7 +1,13 @@
-import {UPDATE_POSITIONS} from '../actions/positions';
+
 import _ from 'lodash';
 import merge from 'lodash/merge';
 
+
+import {UPDATE_POSITIONS} from '../actions/positions';
+
+import {
+  REMOVE_FLIGHTS,
+} from '../actions/flight-list';
 const defaultState = {
   lastFetched: null,
   positions: {}
@@ -9,6 +15,10 @@ const defaultState = {
 
 export default function(state = defaultState, action) {
   switch(action.type) {
+    case REMOVE_FLIGHTS:
+      return Object.assign({}, state, {
+        positions: _.omit(state.positions, action.ifplIds),
+      })
     case UPDATE_POSITIONS:
       return merge({}, state, {
         lastFetched: action.lastFetched,
